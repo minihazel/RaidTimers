8ca7d14ded309dac6fee01a0a7b4f595d97b7f44
minihazel <decyanidez@gmail.com>
2023-01-31 13:10:20 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace RaidTimers
+{
+    partial class raidForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mapsBox = new System.Windows.Forms.ComboBox();
+            this.lblMaps = new System.Windows.Forms.Label();
+            this.lblEscapeTime = new System.Windows.Forms.Label();
+            this.escapeBox = new System.Windows.Forms.TextBox();
+            this.bApply = new System.Windows.Forms.Button();
+            this.lblMapPlaceholder = new System.Windows.Forms.Label();
+            this.bCheck = new System.Windows.Forms.Button();
+            this.chkAllMaps = new System.Windows.Forms.CheckBox();
+            this.boxActionsOptions = new System.Windows.Forms.GroupBox();
+            this.chkReset = new System.Windows.Forms.CheckBox();
+            this.boxActionsOptions.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mapsBox
+            // 
+            this.mapsBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.mapsBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.mapsBox.FormattingEnabled = true;
+            this.mapsBox.Items.AddRange(new object[] {
+            "Customs",
+            "Factory Day",
+            "Factory Night",
+            "Interchange",
+            "Labs",
+            "Lighthouse",
+            "Reserve",
+            "Shoreline",
+            "Streets",
+            "Woods"});
+            this.mapsBox.Location = new System.Drawing.Point(205, 21);
+            this.mapsBox.Name = "mapsBox";
+            this.mapsBox.Size = new System.Drawing.Size(223, 27);
+            this.mapsBox.TabIndex = 0;
+            this.mapsBox.SelectedIndexChanged += new System.EventHandler(this.mapsBox_SelectedIndexChanged);
+            // 
+            // lblMaps
+            // 
+            this.lblMaps.AutoSize = true;
+            this.lblMaps.Location = new System.Drawing.Point(138, 24);
+            this.lblMaps.Name = "lblMaps";
+            this.lblMaps.Size = new System.Drawing.Size(51, 19);
+            this.lblMaps.TabIndex = 1;
+            this.lblMaps.Text = "Maps:";
+            // 
+            // lblEscapeTime
+            // 
+            this.lblEscapeTime.AutoSize = true;
+            this.lblEscapeTime.Location = new System.Drawing.Point(12, 69);
+            this.lblEscapeTime.Name = "lblEscapeTime";
+            this.lblEscapeTime.Size = new System.Drawing.Size(177, 19);
+            this.lblEscapeTime.TabIndex = 4;
+            this.lblEscapeTime.Text = "Escape Time (minutes):";
+            // 
+            // escapeBox
+            // 
+            this.escapeBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.escapeBox.Enabled = false;
+            this.escapeBox.Location = new System.Drawing.Point(205, 66);
+            this.escapeBox.Name = "escapeBox";
+            this.escapeBox.Size = new System.Drawing.Size(223, 27);
+            this.escapeBox.TabIndex = 5;
+            this.escapeBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.escapebox_KeyPress);
+            // 
+            // bApply
+            // 
+            this.bApply.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.bApply.Enabled = false;
+            this.bApply.Font = new System.Drawing.Font("Bahnschrift Light", 10F);
+            this.bApply.Location = new System.Drawing.Point(293, 177);
+            this.bApply.Name = "bApply";
+            this.bApply.Size = new System.Drawing.Size(135, 38);
+            this.bApply.TabIndex = 6;
+            this.bApply.Text = "Apply";
+            this.bApply.UseVisualStyleBackColor = true;
+            this.bApply.Click += new System.EventHandler(this.bApply_Click);
+            // 
+            // lblMapPlaceholder
+            // 
+            this.lblMapPlaceholder.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.lblMapPlaceholder.AutoSize = true;
+            this.lblMapPlaceholder.Location = new System.Drawing.Point(12, 196);
+            this.lblMapPlaceholder.Name = "lblMapPlaceholder";
+            this.lblMapPlaceholder.Size = new System.Drawing.Size(98, 19);
+            this.lblMapPlaceholder.TabIndex = 7;
+            this.lblMapPlaceholder.Text = "Placeholder";
+            this.lblMapPlaceholder.Visible = false;
+            // 
+            // bCheck
+            // 
+            this.bCheck.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.bCheck.Font = new System.Drawing.Font("Bahnschrift Light", 10F);
+            this.bCheck.Location = new System.Drawing.Point(146, 177);
+            this.bCheck.Name = "bCheck";
+            this.bCheck.Size = new System.Drawing.Size(135, 38);
+            this.bCheck.TabIndex = 8;
+            this.bCheck.Text = "Check Time";
+            this.bCheck.UseVisualStyleBackColor = true;
+            this.bCheck.Click += new System.EventHandler(this.bCheck_Click);
+            // 
+            // chkAllMaps
+            // 
+            this.chkAllMaps.AutoSize = true;
+            this.chkAllMaps.Location = new System.Drawing.Point(23, 26);
+            this.chkAllMaps.Name = "chkAllMaps";
+            this.chkAllMaps.Size = new System.Drawing.Size(92, 23);
+            this.chkAllMaps.TabIndex = 9;
+            this.chkAllMaps.Text = "All maps";
+            this.chkAllMaps.UseVisualStyleBackColor = true;
+            this.chkAllMaps.CheckedChanged += new System.EventHandler(this.chkAllMaps_CheckedChanged);
+            // 
+            // boxActionsOptions
+            // 
+            this.boxActionsOptions.Controls.Add(this.chkReset);
+            this.boxActionsOptions.Controls.Add(this.chkAllMaps);
+            this.boxActionsOptions.Location = new System.Drawing.Point(205, 99);
+            this.boxActionsOptions.Name = "boxActionsOptions";
+            this.boxActionsOptions.Size = new System.Drawing.Size(223, 62);
+            this.boxActionsOptions.TabIndex = 10;
+            this.boxActionsOptions.TabStop = false;
+            this.boxActionsOptions.Text = "Options";
+            // 
+            // chkReset
+            // 
+            this.chkReset.AutoSize = true;
+            this.chkReset.Location = new System.Drawing.Point(130, 26);
+            this.chkReset.Name = "chkReset";
+            this.chkReset.Size = new System.Drawing.Size(69, 23);
+            this.chkReset.TabIndex = 10;
+            this.chkReset.Text = "Reset";
+            this.chkReset.UseVisualStyleBackColor = true;
+            this.chkReset.CheckedChanged += new System.EventHandler(this.chkReset_CheckedChanged);
+            // 
+            // raidForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(450, 236);
+            this.Controls.Add(this.boxActionsOptions);
+            this.Controls.Add(this.bApply);
+            this.Controls.Add(this.lblMapPlaceholder);
+            this.Controls.Add(this.bCheck);
+            this.Controls.Add(this.escapeBox);
+            this.Controls.Add(this.lblEscapeTime);
+            this.Controls.Add(this.lblMaps);
+            this.Controls.Add(this.mapsBox);
+            this.Font = new System.Drawing.Font("Bahnschrift Light", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "raidForm";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "RaidTimers";
+            this.Load += new System.EventHandler(this.raidForm_Load);
+            this.boxActionsOptions.ResumeLayout(false);
+            this.boxActionsOptions.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox mapsBox;
+        private System.Windows.Forms.Label lblMaps;
+        private System.Windows.Forms.Label lblEscapeTime;
+        private System.Windows.Forms.TextBox escapeBox;
+        private System.Windows.Forms.Button bApply;
+        private System.Windows.Forms.Label lblMapPlaceholder;
+        private System.Windows.Forms.Button bCheck;
+        private System.Windows.Forms.GroupBox boxActionsOptions;
+        private System.Windows.Forms.CheckBox chkAllMaps;
+        private System.Windows.Forms.CheckBox chkReset;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,431 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RaidTimers
+{
+    public partial class raidForm : Form
+    {
+        public string modDir = Environment.CurrentDirectory;
+        public string modConfig;
+        public string success;
+
+        public raidForm()
+        {
+            InitializeComponent();
+        }
+
+        private void raidForm_Load(object sender, EventArgs e)
+        {
+            modConfig = Path.Combine(modDir, "config\\config.json");
+            if (!File.Exists(modConfig))
+            {
+                mapsBox.Enabled = false;
+                bApply.Enabled = false;
+                MessageBox.Show($"It appears that you have not placed this app in the right folder.\nPlease place RaidTimers.exe and Newtonsoft.Json.dll into the \"RaidTimers\" mod folder.");
+                Application.Exit();
+            } else
+            {
+                this.Text = modConfig;
+            }
+        }
+
+        private void showMessage(string content)
+        {
+            MessageBox.Show(content, this.Text, MessageBoxButtons.OK);
+        }
+
+        private void fetchTime(string item)
+        {
+            string[] mapArray = { };
+            string readConfig = File.ReadAllText(modConfig);
+            JObject obj = JObject.Parse(readConfig);
+            JObject maps = (JObject)obj["maps"];
+
+            try
+            {
+                foreach (var map in maps)
+                {
+                    if (map.Key.ToString() == item.ToLower())
+                    {
+                        string mName = map.Key.ToString();
+                        JObject child = (JObject)map.Value;
+
+                        escapeBox.Text = child["time"].ToString();
+                    }
+                }
+            }
+            catch (Exception err)
+            {
+                Debug.WriteLine($"ERROR: {err.ToString()}");
+            }
+        }
+
+        private void mapsBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (mapsBox.SelectedIndex > -1)
+            {
+                bCheck.Enabled = true;
+                bApply.Enabled = true;
+                escapeBox.Enabled = true;
+
+                switch (mapsBox.SelectedItem.ToString())
+                {
+                    case "Customs":
+                        lblMapPlaceholder.Text = "bigmap";
+                        break;
+                    case "Factory Day":
+                        lblMapPlaceholder.Text = "factory4_day";
+                        break;
+                    case "Factory Night":
+                        lblMapPlaceholder.Text = "factory4_night";
+                        break;
+                    case "Interchange":
+                        lblMapPlaceholder.Text = "interchange";
+                        break;
+                    case "Labs":
+                        lblMapPlaceholder.Text = "laboratory";
+                        break;
+                    case "Lighthouse":
+                        lblMapPlaceholder.Text = "lighthouse";
+                        break;
+                    case "Reserve":
+                        lblMapPlaceholder.Text = "rezervbase";
+                        break;
+                    case "Shoreline":
+                        lblMapPlaceholder.Text = "shoreline";
+                        break;
+                    case "Streets":
+                        lblMapPlaceholder.Text = "tarkovstreets";
+                        break;
+                    case "Woods":
+                        lblMapPlaceholder.Text = "woods";
+                        break;
+                }
+                fetchTime(lblMapPlaceholder.Text);
+            }
+        }
+
+        private void escapebox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) &&
+        (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void bApply_Click(object sender, EventArgs e)
+        {
+            if (chkAllMaps.Checked == true)
+            {
+                if (escapeBox.Text.Contains("."))
+                {
+                    showMessage("Please do not use \".\" periods in the time.");
+                }
+                else
+                {
+                    if (bApply.Text.ToLower().Contains("reset"))
+                    {
+                        if (MessageBox.Show($"Reset time for all maps?", this.Text, MessageBoxButtons.YesNo) == DialogResult.Yes)
+                        {
+                            string readConfig = File.ReadAllText(modConfig);
+                            JObject obj = JObject.Parse(readConfig);
+                            JObject maps = (JObject)obj["maps"];
+
+                            try
+                            {
+                                maps["bigmap"]["time"] = 40;
+                                maps["factory4_day"]["time"] = 20;
+                                maps["factory4_night"]["time"] = 25;
+                                maps["interchange"]["time"] = 40;
+                                maps["laboratory"]["time"] = 35;
+                                maps["lighthouse"]["time"] = 40;
+                                maps["rezervbase"]["time"] = 40;
+                                maps["shoreline"]["time"] = 45;
+                                maps["tarkovstreets"]["time"] = 50;
+                                maps["woods"]["time"] = 40;
+
+                                string output = JsonConvert.SerializeObject(obj, Formatting.Indented);
+
+                                File.WriteAllText(modConfig, output);
+                            }
+                            catch (Exception err)
+                            {
+                                Debug.WriteLine($"ERROR: {err.ToString()}");
+                            }
+                        }
+
+                    } else
+                    {
+                        if (escapeBox.Text.Length > 0 && escapeBox.Text != null)
+                        {
+                            if (MessageBox.Show($"Apply {escapeBox.Text} minutes to all maps?", this.Text, MessageBoxButtons.YesNo) == DialogResult.Yes)
+                            {
+                                string readConfig = File.ReadAllText(modConfig);
+                                JObject obj = JObject.Parse(readConfig);
+                                JObject maps = (JObject)obj["maps"];
+
+                                try
+                                {
+                                    foreach (var map in maps)
+                                    {
+                                        JObject child = (JObject)map.Value;
+                                        child["time"] = Int32.Parse(escapeBox.Text);
+                                    }
+
+                                    string output = JsonConvert.SerializeObject(obj, Formatting.Indented);
+
+                                    File.WriteAllText(modConfig, output);
+                                }
+                                catch (Exception err)
+                                {
+                                    Debug.WriteLine($"ERROR: {err.ToString()}");
+                                }
+                            }
+
+                        }
+                        else
+                        {
+                            showMessage("Please provide a time in minutes to apply to all maps.");
+                        }
+
+                    }
+                }
+
+
+            } else
+            {
+                if (mapsBox.SelectedIndex > -1 && escapeBox.Text != null)
+                {
+                    if (escapeBox.Text.Contains("."))
+                    {
+                        showMessage("Please do not use \".\" periods in the time.");
+                    }
+                    else
+                    {
+                        if (bApply.Text.ToLower().Contains("reset"))
+                        {
+                            if (MessageBox.Show($"Reset time for {mapsBox.SelectedItem.ToString()}?", this.Text, MessageBoxButtons.YesNo) == DialogResult.Yes)
+                            {
+                                string readConfig = File.ReadAllText(modConfig);
+                                JObject obj = JObject.Parse(readConfig);
+                                JObject maps = (JObject)obj["maps"];
+
+                                switch (mapsBox.SelectedItem.ToString().ToLower())
+                                {
+                                    case "customs":
+                                        maps[lblMapPlaceholder.Text]["time"] = 40;
+                                        break;
+
+                                    case "factory day":
+                                        maps[lblMapPlaceholder.Text]["time"] = 20;
+                                        break;
+
+                                    case "factory night":
+                                        maps[lblMapPlaceholder.Text]["time"] = 25;
+                                        break;
+
+                                    case "interchange":
+                                        maps[lblMapPlaceholder.Text]["time"] = 40;
+                                        break;
+
+                                    case "labs":
+                                        maps[lblMapPlaceholder.Text]["time"] = 35;
+                                        break;
+
+                                    case "lighthouse":
+                                        maps[lblMapPlaceholder.Text]["time"] = 40;
+                                        break;
+
+                                    case "reserve":
+                                        maps[lblMapPlaceholder.Text]["time"] = 40;
+                                        break;
+
+                                    case "shoreline":
+                                        maps[lblMapPlaceholder.Text]["time"] = 45;
+                                        break;
+
+                                    case "streets":
+                                        maps[lblMapPlaceholder.Text]["time"] = 50;
+                                        break;
+
+                                    case "woods":
+                                        maps[lblMapPlaceholder.Text]["time"] = 40;
+                                        break;
+                                }
+
+                                string output = JsonConvert.SerializeObject(obj, Formatting.Indented);
+                                File.WriteAllText(modConfig, output);
+
+                            }
+
+                        } else
+                        {
+                            string readConfig = File.ReadAllText(modConfig);
+                            JObject obj = JObject.Parse(readConfig);
+                            JObject maps = (JObject)obj["maps"];
+
+                            try
+                            {
+                                foreach (var map in maps)
+                                {
+                                    if (map.Key.ToString() == lblMapPlaceholder.Text.ToLower())
+                                    {
+                                        if (MessageBox.Show($"Set {escapeBox.Text} minutes of raid time to {mapsBox.SelectedItem.ToString()}?", this.Text, MessageBoxButtons.YesNo) == DialogResult.Yes)
+                                        {
+                                            string mName = map.Key.ToString();
+                                            JObject child = (JObject)map.Value;
+                                            int time = (int)child["time"];
+
+                                            maps[lblMapPlaceholder.Text]["time"] = Int32.Parse(escapeBox.Text);
+
+                                            string output = JsonConvert.SerializeObject(obj, Formatting.Indented);
+                                            File.WriteAllText(modConfig, output);
+                                        }
+                                        break;
+                                    }
+                                    else
+                                    {
+                                        continue;
+                                    }
+                                }
+                            }
+                            catch (Exception err)
+                            {
+                                Debug.WriteLine($"ERROR: {err.ToString()}");
+                            }
+                        }
+
+                    }
+
+                }
+                else
+                {
+                    MessageBox.Show("Please fill out the information necessary.", this.Text, MessageBoxButtons.OK);
+
+                }
+            }
+
+            
+
+        }
+
+        private void bCheck_Click(object sender, EventArgs e)
+        {
+            if (chkAllMaps.Checked == true)
+            {
+                // all maps
+                string[] mapArray = { };
+                string readConfig = File.ReadAllText(modConfig);
+                JObject obj = JObject.Parse(readConfig);
+                JObject maps = (JObject)obj["maps"];
+
+                try
+                {
+                    foreach (var map in maps)
+                    {
+                        string mName = map.Key.ToString();
+                        JObject child = (JObject)map.Value;
+                        int time = (int)child["time"];
+
+                        string content = $"Map {mName} has time: {time.ToString()} minutes\n";
+                        Array.Resize(ref mapArray, mapArray.Length + 1);
+                        mapArray[mapArray.Length - 1] = content;
+                    }
+                }
+                catch (Exception err)
+                {
+                    Debug.WriteLine($"ERROR: {err.ToString()}");
+                }
+
+                try
+                {
+                    MessageBox.Show(string.Join("\n", mapArray), this.Text, MessageBoxButtons.OK);
+
+                }
+                catch (Exception err)
+                {
+                    Debug.WriteLine($"ERROR: {err.ToString()}");
+                }
+
+            }
+            else
+            {
+                // single map
+                if (mapsBox.SelectedIndex > -1 && escapeBox.Text != null && escapeBox.Text.Length > 0)
+                {
+                    string readConfig = File.ReadAllText(modConfig);
+                    JObject obj = JObject.Parse(readConfig);
+                    JObject maps = (JObject)obj["maps"];
+
+                    try
+                    {
+                        foreach (var map in maps)
+                        {
+                            if (map.Key.ToString() == lblMapPlaceholder.Text.ToLower())
+                            {
+                                string mName = map.Key.ToString();
+                                JObject child = (JObject)map.Value;
+                                int time = (int)child["time"];
+
+                                string content = $"Map {mName} has time: {time.ToString()} minutes";
+                                showMessage(content);
+                                break;
+                            }
+                            else
+                            {
+                                continue;
+                            }
+                        }
+                    }
+                    catch (Exception err)
+                    {
+                        Debug.WriteLine($"ERROR: {err.ToString()}");
+                    }
+                } else
+                {
+                    showMessage("Please select a map to view first, alternatively enable \"All maps\"");
+                }
+            }
+        }
+
+        private void chkReset_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkReset.Checked == true)
+            {
+                bApply.Text = "Apply reset";
+            } else
+            {
+                bApply.Text = "Apply";
+            }
+        }
+
+        private void chkAllMaps_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkAllMaps.Checked == true)
+            {
+                bApply.Enabled = true;
+                escapeBox.Enabled = true;
+            } else
+            {
+                if (mapsBox.SelectedIndex < 0)
+                {
+                    bApply.Enabled = false;
+                    escapeBox.Enabled = false;
+                }
+            }
+        }
+    }
+}
